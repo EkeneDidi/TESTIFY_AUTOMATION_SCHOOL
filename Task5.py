7151264b6a6798bdd8c74062344ba352b77cd14a
DidiNworah <ekenendidiamaka@gmail.com>
2024-05-18 15:38:15 +0100

    Python Lessons

---
+++ Task5.py
@@ -0,0 +1,21 @@
+number = 2
+
+# Check if the variable is equal to 2
+if number == 2:
+    print("Number is Two")
+else:
+    print("Number is not Two")
+
+# If the value of the declared variable is changed from two
+number = 5
+
+# it should print "Number is not Two" in the console
+if number == 2:
+    print("Number is Two")
+else:
+    print("Number is not Two")
+
+# if it changed back to 2 it should print "Number is Two" in the console.
+number= 2
+if number ==2:
+    print("Number is Two")
