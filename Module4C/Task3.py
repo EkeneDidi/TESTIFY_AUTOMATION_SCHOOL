94b0e9eb16b36f21acc1b2b254d653bb8250b762
DidiNworah <ekenendidiamaka@gmail.com>
2024-05-25 00:01:11 +0100

    Python Lessons

---
+++ Module4C/Task3.py
@@ -0,0 +1,21 @@
+#Declare a raw string
+#Declare two strings with any value
+#Concatenate the two string
+#Print out the value of the concatenated strings
+
+
+
+life = "It does not matter\nhow slowly you go,\n\tas long as you do not stop!\b"
+# raw string
+life = r"It does not matter\nhow slowly you go,\n\tas long as you do not stop!\b"
+print(life)
+
+# declare two strings with any value
+group = "John"
+attr = 'Paul'
+
+# Concatenate the two string
+name = group + attr
+
+# Print out the value of the concatenated strings
+print(name)
