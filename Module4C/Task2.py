4ac6dfc7548b000bff5914757494cf06e39ab3cc
DidiNworah <ekenendidiamaka@gmail.com>
2024-06-28 16:01:04 +0100

    Python

---
+++ Module4C/Task2.py
@@ -0,0 +1,12 @@
+# Declare a variable with variable_name as my_var and value as 12.6
+
+my_var = 12.6
+print(my_var)
+
+# Declare an integer variable with the value of 200
+number = 200
+print(number)
+
+# Declare a String variable with the value of your name
+myName = 'Ekene'
+print(myName)
